40b0c8d7bf648f0f7b8152fc579dfb28027408a8
ShinyBless <shinybless2019@gmail.com>
2026-08-17 00:15:20 -0300

    adaptación para Icon y efecto hover default anulado

--- src/main/kotlin/dev/maigo/maigoloader/utils/BevelButton.kt
+++ src/main/kotlin/dev/maigo/maigoloader/utils/BevelButton.kt
@@ -2,20 +2,26 @@ package dev.maigo.maigoloader.utils
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import dev.maigo.maigoloader.objects.AppTheme
 
 @Composable
 fun BevelButton(
     text: String,
+    icon: ImageVector? = null, // null por defecto
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     height: Dp = 26.dp,
@@ -25,6 +31,8 @@ fun BevelButton(
     light: Color = AppTheme.Surface2,
     fill: Color = AppTheme.Surface,
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+
     Box(
         modifier = modifier
             .height(height)
@@ -46,10 +54,22 @@ fun BevelButton(
                     .wrapContentWidth()
                     .fillMaxHeight()
                     .background(fill)
-                    .clickable(onClick = onClick)
-                    .padding(horizontal = 12.dp)
+                    .clickable(
+                        onClick = onClick,
+                        interactionSource = interactionSource,
+                        indication = null
+                    )
+                    .padding(horizontal = if (icon != null) 6.dp else 12.dp)
             ) {
-                Text(text, color = textColor, fontSize = fontSize)
+                if (icon != null) {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = text,
+                        tint = textColor,
+                        modifier = Modifier.size(14.dp)
+                    )
+                } else
+                    Text(text, color = textColor, fontSize = fontSize)
             }
         }
     }
